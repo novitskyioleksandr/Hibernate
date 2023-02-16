27a503549132281f7abbbf8249abe4d81ea42992
Novitskyi Oleksandr <novitskyi.oleksandr@gmail.com>
2023-02-16 22:35:04 +0200

    Initial commit

---
+++ src/main/java/Storage/hibernate/HibernateUtil.java
@@ -0,0 +1,36 @@
+package Storage.hibernate;
+import client.Client;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import planet.Planet;
+
+import java.util.List;
+
+public class HibernateUtil {
+    private final SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    private static final HibernateUtil INSTANCE;
+
+    static {
+        INSTANCE = new HibernateUtil();
+    }
+    private HibernateUtil(){
+        sessionFactory= new Configuration()
+                .addAnnotatedClass(Client.class)
+                .addAnnotatedClass(Planet.class)
+                .buildSessionFactory();
+    }
+    public static HibernateUtil getInstance(){
+        return INSTANCE;
+    }
+
+    public void close(){
+        sessionFactory.close();
+    }
+
+    }
+
